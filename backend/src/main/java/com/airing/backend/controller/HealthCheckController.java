19bbc3222ddb4f0213b7ec276e75f33fa3f9b689
do0ori <fuzzydo0ori@gmail.com>
2025-05-22 18:34:21 +0900

    docs: Swagger 문서에 HealthCheckController 태그 추가
    - HealthCheckController에 @Tag(name = "System") annotation 적용
    - 관련 API들을 Swagger UI에서 'System' 그룹으로 구분 가능
    - 참고로 태그명을 지정하지 않으면, Controller의 이름을 Kebab Case(ex: HealthCheckController → health-check-controller)로 변환하여 API를 그룹화됨

--- backend/src/main/java/com/airing/backend/controller/HealthCheckController.java
+++ backend/src/main/java/com/airing/backend/controller/HealthCheckController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Tag(name = "System")
 public class HealthCheckController {
     
     @GetMapping("/health-check")
