42ecd70aa1fae867d2a8f1bff80f56c3a10db292
do0ori <fuzzydo0ori@gmail.com>
2025-05-27 18:00:04 +0900

    feat: 로그인 및 회원가입 화면과 AuthStack 네비게이션 추가
    - AuthStack 내에 LoginScreen, SignUpScreen 구성
    - 로그인 및 회원가입 입력 폼 UI 구현
    - navigation type 정의 (AuthStackParamList)

---
+++ frontend/src/navigation/AuthStack.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import type {AuthStackParamList} from '../types/navigation';
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
+
+const AuthStack = () => (
+  <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="SignUp" component={SignUpScreen} />
+  </Stack.Navigator>
+);
+
+export default AuthStack;
