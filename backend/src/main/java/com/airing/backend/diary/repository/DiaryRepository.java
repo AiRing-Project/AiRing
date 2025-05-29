6e7fa16c37c822b278c075d6e8ca54ddf6dad289
whalsguddlek <whalsguddlek@gmail.com>
2025-05-29 16:44:00 +0900

    feat: 임시 일기 저장

--- backend/src/main/java/com/airing/backend/diary/repository/DiaryRepository.java
+++ backend/src/main/java/com/airing/backend/diary/repository/DiaryRepository.java
@@ -1,6 +1,7 @@
 package com.airing.backend.diary.repository;
 
 import com.airing.backend.diary.entity.Diary;
+<<<<<<< HEAD
 import com.airing.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,10 @@ import java.util.List;
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     List<Diary> findAllByUserAndDateBetween(User user, LocalDate startDate, LocalDate endDate);
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    
+    }
 }
