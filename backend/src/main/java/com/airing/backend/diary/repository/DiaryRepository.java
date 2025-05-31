2d7f8c8afdc9d4681d213deccc1a2d1fd71c43ac
whalsguddlek <whalsguddlek@gmail.com>
2025-05-31 22:46:09 +0900

    feat: presigned-url 발급 및 ImageEntity 저장 기능 구현

--- backend/src/main/java/com/airing/backend/diary/repository/DiaryRepository.java
+++ backend/src/main/java/com/airing/backend/diary/repository/DiaryRepository.java
@@ -1,7 +1,6 @@
 package com.airing.backend.diary.repository;
 
 import com.airing.backend.diary.entity.Diary;
-<<<<<<< HEAD
 import com.airing.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,11 +10,4 @@ import java.util.List;
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     List<Diary> findAllByUserAndDateBetween(User user, LocalDate startDate, LocalDate endDate);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-
-
->>>>>>> 49f4c96 (임시 일기 저장)
 }
