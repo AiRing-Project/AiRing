d2e6e7b40d21a9299fdb6244a116f22283c0d089
whalsguddlek <whalsguddlek@gmail.com>
2025-06-03 00:30:01 +0900

    refactoring: 500 에러 404로 변경

--- backend/src/main/java/com/airing/backend/callLog/service/CallLogService.java
+++ backend/src/main/java/com/airing/backend/callLog/service/CallLogService.java
@@ -5,7 +5,9 @@ import com.airing.backend.callLog.dto.CallLogLatestResponse;
 import com.airing.backend.callLog.entity.CallLog;
 import com.airing.backend.callLog.repository.CallLogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
@@ -28,7 +30,7 @@ public class CallLogService {
 
     public CallLogLatestResponse getLatestCallLog(Long userId) {
         CallLog callLog = callLogRepository.findTopByUserIdOrderByStartedAtDesc(userId)
-                .orElseThrow(() -> new RuntimeException("최근 통화 기록이 없습니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "최근 통화 기록이 없습니다."));
 
         return CallLogLatestResponse.builder()
                 .id(callLog.getId())
